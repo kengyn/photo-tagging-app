3c59255e3c1ed32e704c4293b7175c5f52ced7ed
Ken <knetngyn@gmail.com>
2022-11-19 19:10:47 -0600

    Add closing function for want list

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
+  useEffect(() => {
+    const headerBtn = document.querySelector(".headerBtn");
+    const wantList = document.querySelector(".want-list");
+    const closeModule = () => {
+      if (wantList.classList.contains("closed")) {
+        headerBtn.src = require("../assets/images/arrow-collapse-right.png");
+        wantList.classList.remove("closed");
+        wantList.classList.add("open");
+        headerBtn.classList.remove("closed");
+        headerBtn.classList.add("open");
+      } else {
+        headerBtn.src = require("../assets/images/arrow-collapse-left.png");
+        wantList.classList.remove("open");
+        wantList.classList.add("closed");
+        headerBtn.classList.remove("open");
+        headerBtn.classList.add("closed");
+      }
+    };
+
+    headerBtn.addEventListener("click", closeModule);
+    return () => {
+      headerBtn.removeEventListener("click", closeModule);
+    };
+  });
+
   return (
     <div className="navbar-container">
-      <h1>WANT LIST</h1>
-      <div className="want-list">
+      <img
+        className="headerBtn"
+        src={require("../assets/images/arrow-collapse-right.png")}
+        alt="close"
+      />
+
+      <div className="want-list open">
+        <h1>WANT LIST</h1>
         <div className="character-container">
           <img src={require("../assets/images/eincowboybebop.jpg")} alt="ein" />
           <div>EIN</div>
