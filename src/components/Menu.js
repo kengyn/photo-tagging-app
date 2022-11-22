e89485ce7f71c9964675f58f847bfc42689d1193
Ken <knetngyn@gmail.com>
2022-11-22 16:15:53 -0600

    Add targeting system

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,47 +6,62 @@ const Menu = () => {
   const [left, setLeft] = useState(100);
   useEffect(() => {
     const image = document.querySelector(".main-image");
-    const menu = document.querySelector(".menu-container");
-    function findCoordinate(e) {
-      let x = e.pageX;
-      let y = e.pageY;
-      let left = Math.floor((x / image.clientWidth) * 100);
-      let right = Math.floor((y / image.clientHeight) * 100);
+    const targetMenu = document.querySelector(".target-menu-container");
+    function placeTarget(x, y) {
+      //let left = Math.floor((x / image.clientWidth) * 100);
+      //let right = Math.floor((y / image.clientHeight) * 100);
       setTop(y);
       setLeft(x);
     }
-    //     function placeMenu() {
-    // menu.
-    //     }
 
-    image.addEventListener("click", findCoordinate);
+    function checkHidden(e) {
+      let x = e.pageX;
+      let y = e.pageY;
+      if (targetMenu.style.display === "none") {
+        targetMenu.style.display = "initial";
+        placeTarget(x, y);
+      } else {
+        targetMenu.style.display = "none";
+      }
+    }
+
+    image.addEventListener("click", checkHidden);
 
     return () => {
-      image.removeEventListener("click", findCoordinate);
+      image.removeEventListener("click", checkHidden);
     };
   });
   return (
-    <div className="menu-container" style={{ top: top, left: left }}>
-      <div className="menu-item" id="bob">
-        <img
-          src={require("../assets/images/bob-closeup.webp")}
-          alt="bob closeup"
-        />
-        <p>BOB</p>
-      </div>
-      <div className="menu-item" id="ein">
-        <img
-          src={require("../assets/images/ein-closeup.jpg")}
-          alt="ein closeup"
-        />
-        <p>EIN</p>
-      </div>
-      <div className="menu-item" id="saitama">
-        <img
-          src={require("../assets/images/sai-closeup.jpg")}
-          alt="saitama closeup"
-        />
-        <p>SAITAMA</p>
+    <div className="target-menu-container" style={{ display: "absolute" }}>
+      <div
+        className="target-container"
+        style={{ top: top - 15, left: left - 15 }}
+      ></div>
+      <div
+        className="menu-container"
+        style={{ top: top + 15, left: left + 15 }}
+      >
+        <div className="menu-item" id="bob">
+          <img
+            src={require("../assets/images/bob-closeup.webp")}
+            alt="bob closeup"
+          />
+          <p>BOB</p>
+        </div>
+        <div className="menu-item" id="ein">
+          <img
+            src={require("../assets/images/ein-closeup.jpg")}
+            alt="ein closeup"
+          />
+          <p>EIN</p>
+        </div>
+        <div className="menu-item" id="saitama">
+          <img
+            src={require("../assets/images/sai-closeup.jpg")}
+            alt="saitama closeup"
+          />
+          <p>SAITAMA</p>
+        </div>
       </div>
     </div>
   );
